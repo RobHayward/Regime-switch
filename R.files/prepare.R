43e63a32ba957cef53e26ed227acbc78449ec507
RobHayward <robohay@gmail.com>
2014-04-23 15:24:46 +0100

    Still cannot extract sd from the fm2@response.  Still cannot automate.  I have looked at some of the main currencies vs the EUR.  These are dominated by recent events.  The pdf will not compile with the figures. Seems to be a linux-windows issue.  Check with linux.

--- R.files/prepare.R
+++ R.files/prepare.R
@@ -30,11 +30,11 @@ a <- forp("HUF", "EUR", 1)
 tail(a$data)
 #This is an attempt to create a list of data that can be tested.  The test
 # is in Raw.R
-i <- "HUF"
+i <- "UAH"
 for(i in c("HUF", "PLN")){
   name <- paste(i, "EuR", sep = "")
   name
-  a <- forp(i, "EUR", 1)
+  a <- forp(i, "USD", 1)
   head(a)
   rets <- paste("P", name, sep = "")
   rets
@@ -50,22 +50,19 @@ for(i in c("HUF", "PLN")){
   s1 <- fm2@response[[1]][1]
   s2 <- fm2@response[[2]][1]
   s3 <- fm2@response[[3]][1]
-  
-  pdf("Figures/r", paper= "a4", title = rets)
+  print(c(s1, s2, s3))
+  title <- paste(rets, " Carry Trade", sep = "")
+  pdf("Figures/UAHEUR.pdf", paper= "a4", title = title)
   par(mfrow = c(4,1))
   pst$Date <- index(Data)
   # This just adds a date series for the ppi file (the same as PPLNUSD)
   plot(Data, main = name, type = 'l')
   abline(h = 1)
-  plot(pst[,2] ~ pst$Date, type = 'l', main = "Probability in State 1: Build")
-  plot(pst[,3] ~ pst$Date, type = 'l', main = "Probability in State 2: Crash")
-  plot(pst[,4] ~ pst$Date, type = 'l', main = "Probability in State 3: Caution")
+  plot(pst[,2] ~ pst$Date, type = 'l', main = "Probability in State 1: Crash")
+  plot(pst[,3] ~ pst$Date, type = 'l', main = "Probability in State 2: Caution")
+  plot(pst[,4] ~ pst$Date, type = 'l', main = "Probability in State 3: Build")
   dev.off()
 # This  last bit does not work.  Need to build the list with details.
-  Currency <- name
-  States <- c("S1", "S2", "S3")
-  Dates <- c("dastate1", "dastate2", "dastate3")
-  datalist <- list(c("Currency", "States", "Dates"))
 }
 name
 head(datalist[3])
@@ -76,3 +73,10 @@ datalist[[2]][1][[9]]
 # to choose the second item in list, first row and ninth element.
 plot(datalist$HUF$p, main = "HUFEUR")
 
+fm2@response[[2]][1][1]
+# this will give the mean.  I want sd I don't know how to get that.  
+predict(fm2@response[[3]][[1]])[1]
+
+class(fm2@response)
+names(fm2@response)
+list
