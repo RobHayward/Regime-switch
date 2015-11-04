355f82d0e2145b44ac4bda096c6f630095e01ce2
RobHayward <robohay@gmail.com>
2015-11-04 05:54:16 +0000

    Updated Test.R with some experiments

--- R.files/Package_preparation/Test.R
+++ R.files/Package_preparation/Test.R
@@ -1,6 +1,13 @@
+# create dummy variables
+source("R.files/Package_preparation/FW.R")
+D1 <- rep(0, length(da$VIX))
+D1 <- da$D1[which(da$VIX >= quantile(da$VIX, 0.80)),]          
+
+
+# get the object not the string. 
 currency <- ls(pattern = "???EUR1")
 for(i in currency){
-  (i))
+  (i)
 }
 
 
